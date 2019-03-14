51a341b7f800e676dd195bc7d0419205a45cbf51
liuchaorun <chaorunliu@gmail.com>
2019-03-14 18:26:02 +0800

    fix-can-not-unbind-resource

--- services/screen-management.js
+++ services/screen-management.js
@@ -115,7 +115,7 @@ exports.unbindResourcePack = async (id, screenIds) => {
             }
             if (await user.hasScreen(screen)) {
                 let resource = await screen.getResource();
-                await resource.removeResource(resource, {transaction: t});
+                await resource.removeResources(resource, {transaction: t});
                 let screenData = await screenRedis.get(`screen:${screenId}`);
                 screenData.status = false;
                 screenData.url = null;
